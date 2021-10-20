50587aabcf624d3d964f7179292c7cf827ede6d1
Linah-alkhurayyif <73877590+Linah-alkhurayyif@users.noreply.github.com>
2021-10-20 12:38:27 +0300

    Linah

---
+++ app/src/main/java/com/example/linah_alkhurayyifsingletonpractice/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.linah_alkhurayyifsingletonpractice
+
+object Constants {
+    val currency = arrayListOf("inr", "usd", "aud", "sar", "cny", "jpy")
+    const val tAG = "MainActivity"
+    const val baseURL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"
+}
